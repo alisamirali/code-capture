8268294ebecc4d79cd107f1c21302dc197695545
Ali Samir <alisamir.eng@gmail.com>
2024-02-26 17:32:09 +0200

    Link Shortcut

--- src/components/controls/ExportOptions.jsx
+++ src/components/controls/ExportOptions.jsx
@@ -3,8 +3,8 @@ import {
   ImageIcon,
   Link2Icon,
   Share2Icon,
-} from "@radix-ui/react-icons"
-import { Button } from "../ui/button"
+} from "@radix-ui/react-icons";
+import { Button } from "../ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,84 +12,85 @@ import {
   DropdownMenuSeparator,
   DropdownMenuShortcut,
   DropdownMenuTrigger,
-} from "../ui/dropdown-menu"
-import { toBlob, toPng, toSvg } from "html-to-image"
-import { toast } from "react-hot-toast"
-import useStore from "@/store"
-import { useHotkeys } from "react-hotkeys-hook"
+} from "../ui/dropdown-menu";
+import { toBlob, toPng, toSvg } from "html-to-image";
+import { toast } from "react-hot-toast";
+import useStore from "@/store";
+import { useHotkeys } from "react-hotkeys-hook";
 
 export default function ExportOptions({ targetRef }) {
-  const title = useStore((state) => state.title)
+  const title = useStore((state) => state.title);
 
   const copyImage = async () => {
-    const loading = toast.loading("Copying...")
+    const loading = toast.loading("Copying...");
 
     try {
       const imgBlob = await toBlob(targetRef.current, {
         pixelRatio: 2,
-      })
-      const img = new ClipboardItem({ "image/png": imgBlob })
-      navigator.clipboard.write([img])
+      });
+      const img = new ClipboardItem({ "image/png": imgBlob });
+      navigator.clipboard.write([img]);
 
-      toast.remove(loading)
-      toast.success("Image copied to clipboard!")
+      toast.remove(loading);
+      toast.success("Image copied to clipboard!");
     } catch (error) {
-      toast.remove(loading)
-      toast.error("Something went wrong!")
+      toast.remove(loading);
+      toast.error("Something went wrong!");
     }
-  }
+  };
 
   const copyLink = () => {
     try {
-      const state = useStore.getState()
-      const queryParams = new URLSearchParams({
+      const state = useStore.getState();
+      let queryParams = new URLSearchParams({
         ...state,
         code: btoa(state.code),
-      }).toString()
-      navigator.clipboard.writeText(`${location.href}?${queryParams}`)
+      }).toString();
 
-      toast.success("Link copied to clipboard!")
+      navigator.clipboard.writeText(`${location.href}?${queryParams}`);
+
+      toast.success("Link copied to clipboard!");
     } catch (error) {
-      toast.error("Something went wrong!")
+      toast.error("Something went wrong!");
     }
-  }
+  };
 
   const saveImage = async (name, format) => {
-    const loading = toast.loading(`Exporting ${format} image...`)
+    const loading = toast.loading(`Exporting ${format} image...`);
 
     try {
-      let imgUrl, filename
+      let imgUrl, filename;
       switch (format) {
         case "PNG":
-          imgUrl = await toPng(targetRef.current, { pixelRatio: 2 })
-          filename = `${name}.png`
-          break
+          imgUrl = await toPng(targetRef.current, { pixelRatio: 2 });
+          filename = `${name}.png`;
+          break;
         case "SVG":
-          imgUrl = await toSvg(targetRef.current, { pixelRatio: 2 })
-          filename = `${name}.svg`
-          break
+          imgUrl = await toSvg(targetRef.current, { pixelRatio: 2 });
+          filename = `${name}.svg`;
+          break;
 
         default:
-          return
+          return;
       }
 
-      const a = document.createElement("a")
-      a.href = imgUrl
-      a.download = filename
-      a.click()
+      const a = document.createElement("a");
+      a.href = imgUrl;
+      a.download = filename;
+      a.click();
 
-      toast.remove(loading)
-      toast.success("Exported successfully!")
+      toast.remove(loading);
+      toast.success("Exported successfully!");
     } catch (error) {
-      toast.remove(loading)
-      toast.error("Something went wrong!")
+      toast.remove(loading);
+      toast.error("Something went wrong!");
     }
-  }
+  };
 
-  useHotkeys("ctrl+c", copyImage)
-  useHotkeys("shift+ctrl+c", copyLink)
-  useHotkeys("ctrl+s", () => saveImage(title, "PNG"))
-  useHotkeys("shift+ctrl+s", () => saveImage(title, "SVG"))
+  useHotkeys("ctrl+c", copyImage);
+  useHotkeys("shift+ctrl+c", copyLink);
+  useHotkeys("ctrl+s", () => saveImage(title, "PNG"));
+  useHotkeys("shift+ctrl+s", () => saveImage(title, "SVG"));
 
   return (
     <DropdownMenu>
@@ -129,5 +130,5 @@ export default function ExportOptions({ targetRef }) {
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
